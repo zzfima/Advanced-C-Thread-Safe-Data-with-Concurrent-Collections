c7843e0eaa31225953952691d996fe03506b9af2
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-02-13 09:51:44 +0200

    start

---
+++ ProducerConfsumerCollection/ProducerConfsumerCollection/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProducerConfsumerCollection
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            StackConsumerProducer stackConsumerProducer = new StackConsumerProducer();
+            stackConsumerProducer.Start();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class StackConsumerProducer
+    {
+        ConcurrentStack<int> _stack;
+
+        public StackConsumerProducer()
+        {
+            _stack = new ConcurrentStack<int>();
+        }
+
+        public void Start()
+        {
+            Task.Run(Pop);
+            Task.Run(Push);
+        }
+
+        private void Push()
+        {
+            Random random = new Random();
+            while (true)
+            {
+                _stack.Push(DateTime.Now.Second);
+                Thread.Sleep(random.Next(100, 3000));
+            }
+        }
+
+        public void Pop()
+        {
+            while (true)
+            {
+                var b = _stack.TryPop(out int res);
+                Console.WriteLine($"Success: {b}, res: {res}");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
