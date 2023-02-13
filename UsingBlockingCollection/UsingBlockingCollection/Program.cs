4a8a7d40aca366bd968740525d6170b0286ef05c
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-02-13 13:49:59 +0200

    start

---
+++ UsingBlockingCollection/UsingBlockingCollection/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections.Concurrent;
+var consumer = new BlockingCollectionProducerConsumer();
+Console.ReadKey();
+
+public class BlockingCollectionProducerConsumer
+{
+    BlockingCollection<int> _blockingCollection;
+
+    public BlockingCollectionProducerConsumer()
+    {
+        ConcurrentQueue<int> _queue = new ConcurrentQueue<int>();
+        _blockingCollection = new BlockingCollection<int>(_queue);
+        _blockingCollection.TryAdd(1);
+        _blockingCollection.Add(2);
+        var i = _blockingCollection.Take();
+        i = _blockingCollection.Take();
+        _blockingCollection.TryTake(out i, 2000);
+
+    }
+
+}
